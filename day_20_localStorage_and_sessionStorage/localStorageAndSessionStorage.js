076654cb458448b074c1b5725624061d7e8acc79
ArnabSaga <arnabdey15091@gmail.com>
2024-08-20 07:34:11 +0600

    Day-20 Task Completed

---
+++ day_20_localStorage_and_sessionStorage/localStorageAndSessionStorage.js
@@ -0,0 +1,31 @@
+//-----------Day-20[Local Storage and Session Storage]--------//
+
+
+//Activity-1[Understanding LocalStorage]
+
+//Task-1: Write a script to save a string value to localStorage and retrive it.Log the retrived value.
+
+//Task-2: Write a script to save an object to localStorage by converting it to a JSON string.Retrive and parse the object, then log it.
+
+//Activity-2[Using LocalStorage]
+
+//Task-3: Create a simple form that saves user input (e.g.,name and email) to localStorage when submitted.Retrive and display the saved data on page load.
+//Task-4: Write a script to remove an item from localStorage.Log the localStorage content before and after removal.
+
+
+//Activity-3[Understanding SessionStorage]
+
+//Task-5: Write a script to save a string value to sessionStorage and retrive it.Log the retrived value.
+//Task-6: Write a script to save an object to sessionStorage by converting it to a JSON string. Retrieve and parse the object, then log it.
+
+
+//Activity-4[Using SessionStorage]
+
+//Task-7:Create a simple form that saves user input (e.g.,name and email) to sessionStorage when submitted.Retrieve and display the saved data on page load.
+//Task-8:Write a script to remove an item from sessionStorage.Log the seessionStorage content before and after removal.
+
+
+//Activity-5[Comparing LocalStorage and SessionStorage]
+
+//Task-9:Write a function that accepts a key and value, and saves the value to both localStorage and sessionStorage.Retrieve and log the values from both storage mechanisms.
+//Task-10:Write a function that clears all data from both localStorage and sessionStorage.Verify that both storages are empty.
